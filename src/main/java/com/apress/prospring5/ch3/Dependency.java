17502410a15e23db8973af00bbf625f3c51eea93
antoniocassano84 <antonio.cassano984@gmail.com>
2020-06-10 15:56:20 +0100

    add name to Dependency

--- src/main/java/com/apress/prospring5/ch3/Dependency.java
+++ src/main/java/com/apress/prospring5/ch3/Dependency.java
@@ -2,6 +2,12 @@ package com.apress.prospring5.ch3;
 
 public class Dependency {
 
+  private String name;
+
+  public Dependency(String name) {
+    this.name = name;
+  }
+
   @Override
-  public String toString() { return "Dependency{}"; }
+  public String toString() { return "Dependency{" + this.name + "}"; }
 }
